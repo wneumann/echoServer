654bb5aca56ef187350abde8d366b3a249cc1938
William D. Neumann <wneumann@gmail.com>
2019-10-29 06:47:47 -0500

    Initial Commit

---
+++ echoServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  echoServer
+//
+//  Created by Will Neumann on 10/29/19.
+//  Copyright © 2019 Will Neumann. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
